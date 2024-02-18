af90324b6670c771ffca6d3761d07e4913d7003e
SEAIinitiative <159936079+SEAIinitiative@users.noreply.github.com>
2024-02-18 17:03:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ window.addEventListener('scroll', function() {
 
 const navigation = document.querySelector('nav');
 document.querySelector('.toggle').onclick = function(){
-    this.classList.toggle('active');
-    navigation.classList.toggle('active');
-}
+        this.classList.toggle('spin');  //adds a spin function after click
+    setTimeout(() => {
+          // this removes spin funciton
+        this.classList.toggle('active');
+        navigation.classList.toggle('active');
+    }, 123);//this is the start of a delay 
+    setTimeout(() => {
+        this.classList.toggle('spin');
+    },123)
+};
